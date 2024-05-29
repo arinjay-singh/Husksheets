545bb87d2aeafe49d0c3d4567735f38a42629b5d
Arinjay Singh <arinjay.singh@gmail.com>
2024-05-29 16:41:17 -0400

    updating directory

---
+++ Backend/API/auth.java
@@ -0,0 +1,8 @@
+/*The server uses Basic authentication:
+you should send a header with key Authorization
+and the value Basic auth,
+where auth is username:password encoded with base 64.
+The set of client names and passwords is pre-assigned.
+Status code 401 is returned when the request is not authorized. HTTPS is used.
+*/
+
