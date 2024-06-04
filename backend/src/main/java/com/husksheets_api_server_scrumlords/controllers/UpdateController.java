cab6da5ac250f83b45a16e6f0ea0228ca6fe0ecc
parnikajain <20jainparnika@gmail.com>
2024-06-04 17:08:18 -0400

    updated file names

--- backend/src/main/java/com/husksheets_api_server_scrumlords/controllers/UpdateController.java
+++ backend/src/main/java/com/husksheets_api_server_scrumlords/controllers/UpdateController.java
@@ -22,18 +22,24 @@ public class UpdateController {
     }
 
     @PostMapping("api/v1/updatePublished")
-    public Response updateSheet(@RequestBody UpdateRequest request) {
+    public Response updatePublished(@RequestBody UpdateRequest request) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String username = auth.getName();
+        String owner = request.getPublisher();
+        if (!owner.equals(username)) {
+            return new Response(false, "Unauthorized: sender is not owner of sheet");
+        }
         Publisher publisher = publishers.getPublisher(username);
+        if (publisher == null) {
+            return new Response(false, "Not found:" + request.getPublisher() +
+                    "/" + request.getSheet());
+        }
         Sheet userSheet = publisher.getSheets().stream()
                 .filter(s -> request.getSheet().equals(s.getSheet())).findAny().orElse(null);
-        Response check = checkAuthSheet(username, request, userSheet);
-        if (check != null) {
-            return check;
-        }
-        else {
-            //assert userSheet != null;
+        if (userSheet == null) {
+            return new Response(false, "Not found:" + request.getPublisher() +
+                    "/" + request.getSheet());
+        } else {
             userSheet.setPayload(request.getPayload());
             return new Response(true, null);
         }
@@ -41,27 +47,22 @@ public class UpdateController {
 
     @PostMapping("api/v1/updateSubscription")
     public Response updateSubscription(@RequestBody UpdateRequest request) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
-        Publisher publisher = publishers.getPublisher(username);
+        String owner = request.getPublisher();
+        Publisher publisher = publishers.getPublisher(owner);
+        if (publisher == null) {
+            return new Response(false, "Not found:" + request.getPublisher() +
+                    "/" + request.getSheet());
+        }
         Sheet userSheet = publisher.getSheets().stream()
                 .filter(s -> request.getSheet().equals(s.getSheet())).findAny().orElse(null);
-        Response check = checkAuthSheet(username, request, userSheet);
-        if (check != null) {
-            return check;
-        }
-        else {
-            //something goes here for sheet update
+        if (userSheet == null) {
+            return new Response(false, "Not found:" + request.getPublisher() +
+                    "/" + request.getSheet());
+        } else {
+            userSheet.setPayload(request.getPayload());
             return new Response(true, null);
         }
     }
 
-    private Response checkAuthSheet(String authUsername, UpdateRequest request, Sheet userSheet) {
-        if (!authUsername.equals(request.getPublisher()) || userSheet == null) {
-            return new Response(false, "Not found:" + request.getPublisher() +
-                    "/" + request.getSheet());
-        }
-        else return null;
-    }
 
 }
