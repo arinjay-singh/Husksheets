ae0ea214754889a96121c5a11a6966e215ce23d7
parnikajain <20jainparnika@gmail.com>
2024-06-08 16:08:51 -0400

    commented out register

--- client/src/app/api/register.tsx
+++ client/src/app/api/register.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 
 const axiosClient = axios.create();
 
@@ -11,3 +11,4 @@ export async function getRequest(URL: string) {
 
     return response.data
 }
+*/
